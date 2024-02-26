3fc11ca6c0e1bf391d5ef34e06167b79de19bc74
gayathrikk <gayathri@htic.iitm.ac.in>
2024-02-26 12:22:08 +0530

    update commit

--- src/test/java/Error/Console_log/Atlas_consolelog.java
+++ src/test/java/Error/Console_log/Atlas_consolelog.java
@@ -130,34 +130,53 @@ public class Atlas_consolelog {
 	
 			
 	    	 try {
-	    		    WebElement Mouse = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@class='cdk-table nb-tree-grid']//tr[6]")));
+	    		    WebElement Mouse = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@role='grid']//tr[6]")));
 	    		    Mouse.click();
 	    		    System.out.println("Human Fetus clicked successfully.");
 	    		    Thread.sleep(2000);
 	    		} catch (Exception e) {
 	    		    System.out.println("Human Fetus not clicked: " + e.getMessage());
 	    		}
-	    	 
+	    	 checkConsoleLog();
 	    	 
 	    	 try {
-	    		    WebElement Brain = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@class='cdk-table nb-tree-grid']//tr[7]//td[2]")));
+	    		    WebElement Brain = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@role='grid']//tr[7]//td[2]")));
 	    		    Brain.click();
 	    		    System.out.println(" Brain clicked successfully.");
 	    		    Thread.sleep(2000);
 	    		} catch (Exception e) {
 	    		    System.out.println("Brain not clicked: " + e.getMessage());
 	    		}
-	    	
+	    	 checkConsoleLog();
 	    	 
 	    	 try {
-	 		    WebElement MTB_10 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@class='cdk-table nb-tree-grid']//tr[25]//td[3]")));
+	 		    WebElement MTB_10 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@role='grid']//tr[25]//td[3]")));
 	 		    MTB_10.click();
 	 		    System.out.println("FTB-40 clicked successfully.");
 	 		    Thread.sleep(2000);
 	 		} catch (Exception e) {
 	 		    System.out.println(" FTB-40 not clicked: " + e.getMessage());
 	 		}
-	    	
+	    	 checkConsoleLog();
+	    	 
+	    	 String parentWindow = driver.getWindowHandle();
+	    	 try {
+		 		    WebElement viewericon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//nb-icon[@nbtooltip='Viewer']")));
+		 		   viewericon.click();
+		 		    System.out.println("viewer icon is clicked successfully.");
+		 		    Thread.sleep(2000);
+		 		} catch (Exception e) {
+		 		    System.out.println(" viewer icon is not clicked: " + e.getMessage());
+		 		}
+	    	 
+	    	 wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+			  Set<String> allWindows = driver.getWindowHandles();
+		        for (String window : allWindows) {
+		            if (!window.equals(parentWindow)) {
+		                driver.switchTo().window(window);
+		                break;
+		            }
+		        }
 	    	 
 	    	 try {
 	  		    WebElement section = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[text()='883'])[1]")));
@@ -168,12 +187,11 @@ public class Atlas_consolelog {
 	  		    System.out.println("section not clicked: " + e.getMessage());
 	  		}
 	    	 
+	    	 checkConsoleLog();
 	    	
-	    	 
 	    	 System.out.println("************************************Series set validation done********************************");
 	
-	    	 
-		}
+	 	}
 		
 		@Test(priority=3)
 		
